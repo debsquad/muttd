b2abbc81831aae3435ea11c619a17d445d938b05
Bertrand Janin <b@janin.com>
2014-12-18 16:38:41 -0500

    Automatically handle mimetypes using the appropriate module.

--- muttd.py
+++ muttd.py
@@ -2,6 +2,7 @@
 
 import urllib
 import os
+import mimetypes
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from os import curdir, sep
@@ -25,49 +26,24 @@ class MuttdHandler(BaseHTTPRequestHandler):
         if self.path == "/":
             self.path = "/index.html"
 
-        sendReply = False
-        if self.path.endswith(".txt"):
-            mimetype = "text/plain"
-            sendReply = True
-        elif self.path.endswith(".html"):
-            mimetype = "text/html"
-            sendReply = True
-        elif self.path.endswith(".jpg"):
-            mimetype = "image/jpg"
-            sendReply = True
-        elif self.path.endswith(".gif"):
-            mimetype = "image/gif"
-            sendReply = True
-        elif self.path.endswith(".png"):
-            mimetype = "image/png"
-            sendReply = True
-        elif self.path.endswith(".js"):
-            mimetype = "application/javascript"
-            sendReply = True
-        elif self.path.endswith(".css"):
-            mimetype = "text/css"
-            sendReply = True
-
-        if not sendReply:
+        mimetype, _ = mimetypes.guess_type(self.path)
+        if not mimetype:
             mimetype = "application/octet-stream"
-            sendReply = True
 
-        # Open the static file requested and send it
-        if sendReply:
-            # unquote() handles URL encoding and basename is a quick way to
-            # avoid users from requesting files outside of the path (for
-            # example ../../../../etc/passwd)
-            path = basename(urllib.unquote(self.path))
-            try:
-                f = open(curdir + sep + path)
-            except IOError:
-                self.send_error(404, "File Not Found: {}".format(self.path))
-                return
-            self.send_response(200)
-            self.send_header("Content-type", mimetype)
-            self.end_headers()
-            self.wfile.write(f.read())
-            f.close()
+        # unquote() handles URL encoding and basename is a quick way to
+        # avoid users from requesting files outside of the path (for
+        # example ../../../../etc/passwd)
+        path = basename(urllib.unquote(self.path))
+        try:
+            f = open(curdir + sep + path)
+        except IOError:
+            self.send_error(404, "File Not Found: {}".format(self.path))
+            return
+        self.send_response(200)
+        self.send_header("Content-type", mimetype)
+        self.end_headers()
+        self.wfile.write(f.read())
+        f.close()
 
 
 if not os.path.exists(DIR):
